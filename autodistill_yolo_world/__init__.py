56fe25d40ab713f948184f5a508ead2a6e66e32e
Lakshman <lakshman@moiiai.com>
2024-02-15 10:44:50 +0100

    Add Makefile, autodistill_yolo_world module, and setup.py

---
+++ autodistill_yolo_world/__init__.py
@@ -0,0 +1,1 @@
+from autodistill_yolo_world.yolo_world import YOLOWorld
---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+from setuptools import find_packages
+import re
+
+with open("./autodistill_owl_vit/__init__.py", 'r') as f:
+    content = f.read()
+    # from https://www.py4u.net/discuss/139845
+    version = re.search(
+        r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content).group(1)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autodistill_yolo_world",
+    version=version,
+    author="Lakshman",
+    author_email="lakshman@moiiai.com",
+    description="Yolo-World module for use with Autodistill",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lakshmanmoiiai/autodistill-yolo-world",
+    install_requires=[
+        "torch",
+        "supervision",
+        "numpy",
+        "transformers"
+    ],
+    packages=find_packages(exclude=("tests",)),
+    extras_require={
+        "dev": ["flake8", "black==22.3.0", "isort", "twine", "pytest", "wheel"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
