953cc581fe348bab36f6e92800884330ab2f9936
Lakshman <lakshman@moiiai.com>
2024-02-15 21:22:42 +0100

    updated

--- setup.py
+++ setup.py
@@ -21,7 +21,14 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lakshmanmoiiai/autodistill-yolo-world",
-    install_requires=["torch", "supervision", "numpy", "ultralytics", "autodistill"],
+    install_requires=[
+        "torch",
+        "supervision",
+        "numpy",
+        "ultralytics",
+        "autodistill",
+        "roboflow",
+    ],
     packages=find_packages(exclude=("tests",)),
     extras_require={
         "dev": ["flake8", "black==22.3.0", "isort", "twine", "pytest", "wheel"],
